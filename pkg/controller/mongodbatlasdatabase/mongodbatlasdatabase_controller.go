1ee8ff1d8d3f61b87189ce47b2740b27b153313f
saurabhbali <saurabhbali9797@gmail.com>
2021-03-06 19:06:02 +0530

    Added logic for database controller

--- pkg/controller/mongodbatlasdatabase/mongodbatlasdatabase_controller.go
+++ pkg/controller/mongodbatlasdatabase/mongodbatlasdatabase_controller.go
@@ -2,21 +2,29 @@ package mongodbatlasdatabase
 
 import (
 	"context"
-
+	"fmt"
+	"strconv"
+	"time"
+	"reflect"
+	
 	knappekv1alpha1 "github.com/saurabhbali/mongodbatlas-operator/pkg/apis/knappek/v1alpha1"
-	corev1 "k8s.io/api/core/v1"
+	//corev1 "k8s.io/api/core/v1"
+	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
+	//"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	//"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+	// "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var log = logf.Log.WithName("controller_mongodbatlasdatabase")
@@ -26,6 +34,22 @@ var log = logf.Log.WithName("controller_mongodbatlasdatabase")
 * business logic.  Delete these comments after modifying this file.*
  */
 
+ // ReconciliationConfig let us customize reconcilitation
+type ReconciliationConfig struct {
+	Time time.Duration
+}
+
+// GetReconcilitationConfig gives us default values
+func GetReconcilitationConfig() *ReconciliationConfig {
+	// default reconciliation loop time is 2 minutes
+	timeString := "120"
+	timeInt, _ := strconv.Atoi(timeString)
+	reconciliationTime := time.Second * time.Duration(timeInt)
+	return &ReconciliationConfig{
+		Time: reconciliationTime,
+	}
+}
+
 // Add creates a new MongoDBAtlasDatabase Controller and adds it to the Manager. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
 func Add(mgr manager.Manager) error {
@@ -34,7 +58,7 @@ func Add(mgr manager.Manager) error {
 
 // newReconciler returns a new reconcile.Reconciler
 func newReconciler(mgr manager.Manager) reconcile.Reconciler {
-	return &ReconcileMongoDBAtlasDatabase{client: mgr.GetClient(), scheme: mgr.GetScheme()}
+	return &ReconcileMongoDBAtlasDatabase{client: mgr.GetClient(), scheme: mgr.GetScheme(), reconciliationConfig: GetReconcilitationConfig()}
 }
 
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
@@ -47,16 +71,7 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 
 	// Watch for changes to primary resource MongoDBAtlasDatabase
 	err = c.Watch(&source.Kind{Type: &knappekv1alpha1.MongoDBAtlasDatabase{}}, &handler.EnqueueRequestForObject{})
-	if err != nil {
-		return err
-	}
-
-	// TODO(user): Modify this to be the types you create that are owned by the primary resource
-	// Watch for changes to secondary resource Pods and requeue the owner MongoDBAtlasDatabase
-	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
-		IsController: true,
-		OwnerType:    &knappekv1alpha1.MongoDBAtlasDatabase{},
-	})
+// err = c.Watch(&source.Kind{Type: &MongoDBAtlasDatabase{}}, &handler.EnqueueRequestForObject{})
 	if err != nil {
 		return err
 	}
@@ -73,6 +88,7 @@ type ReconcileMongoDBAtlasDatabase struct {
 	// that reads objects from the cache and writes to the apiserver
 	client client.Client
 	scheme *runtime.Scheme
+	reconciliationConfig *ReconciliationConfig
 }
 
 // Reconcile reads that state of the cluster for a MongoDBAtlasDatabase object and makes changes based on the state read
@@ -88,6 +104,7 @@ func (r *ReconcileMongoDBAtlasDatabase) Reconcile(request reconcile.Request) (re
 
 	// Fetch the MongoDBAtlasDatabase instance
 	instance := &knappekv1alpha1.MongoDBAtlasDatabase{}
+	// instance := &MongoDBAtlasDatabase{}
 	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
 	if err != nil {
 		if errors.IsNotFound(err) {
@@ -100,54 +117,86 @@ func (r *ReconcileMongoDBAtlasDatabase) Reconcile(request reconcile.Request) (re
 		return reconcile.Result{}, err
 	}
 
-	// Define a new Pod object
-	pod := newPodForCR(instance)
-
-	// Set MongoDBAtlasDatabase instance as the owner and controller
-	if err := controllerutil.SetControllerReference(instance, pod, r.scheme); err != nil {
-		return reconcile.Result{}, err
-	}
-
-	// Check if this Pod already exists
-	found := &corev1.Pod{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, found)
-	if err != nil && errors.IsNotFound(err) {
-		reqLogger.Info("Creating a new Pod", "Pod.Namespace", pod.Namespace, "Pod.Name", pod.Name)
-		err = r.client.Create(context.TODO(), pod)
+	// Create a new MongoDBAtlasDatabase
+	isMongoDBAtlasDatabaseToBeCreated := reflect.DeepEqual(instance.Status, knappekv1alpha1.MongoDBAtlasDatabaseStatus{})
+	if isMongoDBAtlasDatabaseToBeCreated {
+		// err = createMongoDBAtlasDatabaseUser(reqLogger, r.atlasClient, atlasDatabaseUser, atlasProject)
+		err = createMongoDBAtlasDatabase(reqLogger, instance)
 		if err != nil {
 			return reconcile.Result{}, err
 		}
-
-		// Pod created successfully - don't requeue
-		return reconcile.Result{}, nil
-	} else if err != nil {
-		return reconcile.Result{}, err
+		err = r.client.Status().Update(context.TODO(), instance)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+		return reconcile.Result{RequeueAfter: r.reconciliationConfig.Time}, nil
 	}
+	// // Check if this Pod already exists
+	// found := &corev1.Pod{}
+	// err = r.client.Get(context.TODO(), types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, found)
+	// if err != nil && errors.IsNotFound(err) {
+	// 	reqLogger.Info("Creating a new Pod", "Pod.Namespace", pod.Namespace, "Pod.Name", pod.Name)
+	// 	err = r.client.Create(context.TODO(), pod)
+	// 	if err != nil {
+	// 		return reconcile.Result{}, err
+	// 	}
 
-	// Pod already exists - don't requeue
-	reqLogger.Info("Skip reconcile: Pod already exists", "Pod.Namespace", found.Namespace, "Pod.Name", found.Name)
-	return reconcile.Result{}, nil
+	// 	// Pod created successfully - don't requeue
+	// 	return reconcile.Result{}, nil
+	// } else if err != nil {
+	// 	return reconcile.Result{}, err
+	// }
+	// // Pod already exists - don't requeue
+	// reqLogger.Info("Skip reconcile: Pod already exists", "Pod.Namespace", found.Namespace, "Pod.Name", found.Name)
+	return reconcile.Result{RequeueAfter: r.reconciliationConfig.Time}, nil
 }
 
 // newPodForCR returns a busybox pod with the same name/namespace as the cr
-func newPodForCR(cr *knappekv1alpha1.MongoDBAtlasDatabase) *corev1.Pod {
-	labels := map[string]string{
-		"app": cr.Name,
-	}
-	return &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      cr.Name + "-pod",
-			Namespace: cr.Namespace,
-			Labels:    labels,
-		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:    "busybox",
-					Image:   "busybox",
-					Command: []string{"sleep", "3600"},
-				},
-			},
-		},
-	}
+// func newPodForCR(cr *knappekv1alpha1.MongoDBAtlasDatabase) *corev1.Pod {
+// 	labels := map[string]string{
+// 		"app": cr.Name,
+// 	}
+// 	return &corev1.Pod{
+// 		ObjectMeta: metav1.ObjectMeta{
+// 			Name:      cr.Name + "-pod",
+// 			Namespace: cr.Namespace,
+// 			Labels:    labels,
+// 		},
+// 		Spec: corev1.PodSpec{
+// 			Containers: []corev1.Container{
+// 				{
+// 					Name:    "busybox",
+// 					Image:   "busybox",
+// 					Command: []string{"sleep", "3600"},
+// 				},
+// 			},
+// 		},
+// 	}
+// }
+
+func createMongoDBAtlasDatabase(reqLogger logr.Logger, cr *knappekv1alpha1.MongoDBAtlasDatabase) error {
+// func createMongoDBAtlasDatabase(reqLogger logr.Logger, cr *MongoDBAtlasDatabase) error {
+	hosta := cr.Spec.Host
+	clientdb, err := mongo.NewClient(options.Client().ApplyURI(hosta))
+    if err != nil {
+        return fmt.Errorf("Error creating new client %s", err)
+    }
+    ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+    err = clientdb.Connect(ctx)
+    if err != nil {
+        return fmt.Errorf("Error connecting Atlas cluster %s", err)
+    }
+    defer clientdb.Disconnect(ctx)
+
+    quickstartDatabase := clientdb.Database(cr.Spec.Database)
+    coll := quickstartDatabase.Collection("test")
+	_ = coll
+	return updateCRStatus(reqLogger, cr)
+}
+
+func updateCRStatus(reqLogger logr.Logger, cr *knappekv1alpha1.MongoDBAtlasDatabase) error {
+	// update status field in CR
+	cr.Status.Created = "created"
+	cr.Status.Database = cr.Spec.Database
+	return nil
 }
